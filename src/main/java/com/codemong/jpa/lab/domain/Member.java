cc2e1004ac1de358209f3ce5f1a3e50fc7f9aa6b
YunSungBin <dbstjdqls14@naver.com>
2026-06-25 01:16:37 +0900

    Initialize answer code: src/main/java/com/codemong/jpa/lab/domain/Member.java

---
+++ src/main/java/com/codemong/jpa/lab/domain/Member.java
@@ -0,0 +1,30 @@
+package com.codemong.jpa.lab.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    public Member(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+}
